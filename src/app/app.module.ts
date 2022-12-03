38a042f9573037b2eda4435d6e049cd6d704c836
EmoHei <98215816+EmoHei@users.noreply.github.com>
2022-12-03 11:34:06 +0100

    start new project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthModule } from './auth/auth.module';
+import { CoreModule } from './core/core.module';
+import { SharedModule } from './shared/shared.module';
+import { TourModule } from './tour/tour.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +14,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AuthModule,
+    CoreModule,
+    SharedModule,
+    TourModule
   ],
   providers: [],
   bootstrap: [AppComponent]
